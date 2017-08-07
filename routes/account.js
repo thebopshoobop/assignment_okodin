025b3a5b835c5dd4dd058e8f177dc4fd7d592846
Will Timpson <will.timpson@gmail.com>
2017-08-07 15:19:23 -0400

    Refactor account

--- routes/account.js
+++ routes/account.js
@@ -9,41 +9,47 @@ router.get("/", (req, res) => {
   let sessionId = req.session.id;
   if (sessionId === undefined) {
     return res.render(`loginPage`);
+  } else {
+    User.find({ where: { id: sessionId } })
+      .then(result => {
+        if (result.loggedIn) res.redirect(`/profile/${sessionId}`);
+        else res.render("loginPage");
+      })
+      .catch(e => {
+        res.status(500).send(e.stack);
+      });
   }
-  User.find({where:{ id: sessionId }}).then(result => {
-    if (result.loggedIn === true) {
-      return res.redirect(`/profile/${sessionId}`);
-    }
-    res.render('loginPage');
-  }).catch(e => res.status(500).send(e.stack)););
 });
 
-
-
-router.post("/loginPage", (req,res)=>{
+router.post("/login", (req, res) => {
   //TODO: make username unique in DB
   //and give index
   let loggedUser = req.body.username;
-  User.find({where:{username: loggedUser}}).then(result => {
-      if(result.id){ return res.redirect(`/profile/${result.id}`);
-          User.update({loggedIn = true},{where:{id: result.id },limit: 1});
-        }
-      else{req.flash('error',"Sorry that username doesnt exist, blame Loki");
-          res.redirect("/");}
-                                                            }
-                                                          ).catch(e => res.status(500).send(e.stack)););;
-
-
-})
-router.post("/register", (req,res)=>{
-  let userParams = {"username": req.body.username};
-  sequelize.transaction(
-    t => {return User.findOrCreate({defaults: userParams, where: {username: userParams.username },transaction:t})
-    .spread((result,created) => {
-        if(!created){  req.flash('error',"Sorry that username DOES exist, blame Loki");
-        res.redirect("/");  }
-        else{//TODO: log user in because we just created them then redirect to profile  }
-           })
-
-
-})
+  User.find({ where: { username: loggedUser } })
+    .then(result => {
+      if (result.id) {
+        User.update({ loggedIn: true }, { where: { id: result.id }, limit: 1 });
+        res.redirect(`/profile/${result.id}`);
+      } else {
+        req.flash("error", "Sorry that username doesnt exist, blame Loki");
+        res.redirect("/");
+      }
+    })
+    .catch(e => res.status(500).send(e.stack));
+});
+router.post("/register", (req, res) => {
+  let userParams = { username: req.body.username };
+  User.findOrCreate({
+    defaults: userParams,
+    where: { username: userParams.username }
+  })
+    .spread((result, created) => {
+      if (!created) {
+        req.flash("error", "Sorry that username DOES exist, blame Loki");
+        res.redirect("/");
+      } else {
+        //TODO: log user in because we just created them then redirect to profile
+      }
+    })
+    .catch(e => res.status(500).send(e.stack));
+});
